6374d51182616ba8898fa37383c93d70a1807511
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-04-28 23:29:02 -0300

    wip

--- src/ummoi/core.clj
+++ src/ummoi/core.clj
@@ -1,1 +1,14 @@
-(ns ummoi.core)
+(ns ummoi.core
+  (:require
+   [clojure.java.shell :as sh]
+   [tla-edn.core :as tla-edn]))
+
+#_(sh/sh)
+
+"um -cfg p.edn"
+
+'{:operators
+  {"TransferMoney"
+   {:args [x y z]
+    :call {:type :shell
+           :command ["bb" "a.clj" x y z]}}}}
