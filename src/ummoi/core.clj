e98ebe060ffa562af228cbf11bb3962475dd5544
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-04-30 01:17:57 -0300

    nowhere

--- src/ummoi/core.clj
+++ src/ummoi/core.clj
@@ -18,22 +18,39 @@
            :command ["bb" "a.clj" x y z]}}}}
 
 #_(spec/defop TransferMoney {:module "example"}
-  [_ _ _ ])
+    [_ _ _ ])
 
 (defmacro sss
-  [n]
+  [n & body]
   `(spec/defop ~'TransferMoney {:module "example"}
-     ~(vec (repeat n '_))))
+    ~(vec (repeat n '_))
+    ~@body))
 
-#_(defn aaa
+(def vars-keys
+  [:c1 :c2 :account :receiver-new-amount :sender-new-amount :sender
+   :receiver :money :pc])
+
+#_(sss 3
+     (tla-edn/to-tla-value {:c1 10 :c2 10}))
+
+(macroexpand-1
+ '(sss 3 3))
+
+#_(eval `(sss 3))
+
+#_(defn xxx
   [n]
-  (println :NNN n)
-  (eval ('list `sss n)))
+  (eval
+   (macroexpand
+    `(sss 3
+          (tla-edn/to-tla-value {:c1 10 :c2 10})))))
 
 (defn -main
   [& [n]]
   #_(aaa n)
-  (eval `(sss ~(bigdec n)))
+  #_(eval `(sss 3))
+  (eval `(sss 3 (tla-edn/to-tla-value {:c1 10 :c2 10})))
+  #_(xxx n)
   (println :CLASSES @spec/classes-to-be-loaded)
   (println :LOADED_BEFORE? (spec/classes-loaded?))
   (spec/compile-operators 'ummoi.core)
