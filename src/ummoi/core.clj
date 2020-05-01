312078876d94d595427ebbe4cc028d6e9d0af0c4
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-01 15:19:46 -0300

    fff

--- src/ummoi/core.clj
+++ src/ummoi/core.clj
@@ -1,10 +1,13 @@
 (ns ummoi.core
-  (:gen-class)
+  #_(:gen-class)
   (:require
+   [borkdude.deps :as deps]
    [clojure.java.shell :as sh]
+   [clojure.pprint :as pp]
+   [me.raynes.fs :as fs]
    [tla-edn.core :as tla-edn]
    [tla-edn.spec :as spec]
-   [clojure.pprint :as pp])
+   [clojure.string :as str])
   (:import
    (java.io File)))
 
@@ -12,17 +15,19 @@
 
 #_"um -cfg p.edn"
 
-#_'{:operators
+(def ummoi-config
+  '{:operators
     {"TransferMoney"
-     {:args [x y z]
-      :call {:type :shell
-             :command ["bb" "a.clj" x y z]}}}}
+     {:module "example"
+      :args [a b c]
+      :run {:type :shell
+            :command ["bb" "a.clj" x y z]}}}})
 
 (def vars-keys
   [:c1 :c2 :account :receiver-new-amount :sender-new-amount :sender
    :receiver :money :pc])
 
-(spec/defop TransferMoney {:module "example"}
+#_(spec/defop TransferMoney {:module "example"}
   [self account vars]
   (let [self (tla-edn/to-edn self)
         vars (zipmap vars-keys (tla-edn/to-edn vars))
@@ -34,8 +39,70 @@
         (update receiver + money)
         tla-edn/to-tla-value)))
 
+#_(spec/defop TransferMoney {:module "example"}
+  [self account vars]
+  (let [self (tla-edn/to-edn self)
+        vars (zipmap vars-keys (tla-edn/to-edn vars))
+        sender (get-in vars [:sender self])
+        receiver (get-in vars [:receiver self])
+        money (get-in vars [:money self])]
+    (-> (tla-edn/to-edn account)
+        (update sender + money)
+        (update receiver + money)
+        tla-edn/to-tla-value)))
+
+(defn pp-spit
+  [file data]
+  (spit file (with-out-str (pp/pprint data))))
+
+(defn op-form
+  [{:keys [:name :module :args]}]
+  `(spec/defop ~(symbol name) {:module ~module}
+     ~args
+     10))
+
+(defn core-form
+  [op-forms]
+  (->>
+   `[(~'ns ummoi-runner.core
+      ~'(:require
+         [clojure.java.shell :as sh]
+         [tla-edn.core :as tla-edn]
+         [tla-edn.spec :as spec]))
+
+     ~@op-forms
+
+     (defn ~'-main
+       []
+       (spec/run-spec "/home/rafael/dev/ummoi/resources/example.tla"
+                      #_(.getAbsolutePath (File. "resources/example.tla"))
+                      "example.cfg")
+       (System/exit 0))]
+   (map #(with-out-str (pp/pprint %)))
+   (str/join "\n")))
+
+(def op-forms
+  (mapv (fn [[name {:keys [:args :module :run]}]]
+          (op-form {:name name :args args :module module}))
+        (:operators ummoi-config)))
+
+(def deps-config
+  '{:deps {org.clojure/clojure {:mvn/version "1.10.1"}
+           pfeodrippe/tla-edn {:mvn/version "0.3.0"}}
+    :paths ["src" "classes"]})
+
+(def my-ns *ns*)
+
 (defn -main
   []
-  (spec/run-spec (.getAbsolutePath (File. "resources/example.tla"))
+  (let [dir (bean (fs/temp-dir "ummoi-"))
+        _ (fs/mkdirs (str (:path dir) "/src/ummoi_runner"))
+        deps-file (str (:path dir) "/deps.edn")
+        core-file (str (:path dir) "/src/ummoi_runner/core.clj")]
+    (pp-spit deps-file deps-config)
+    (spit core-file (core-form op-forms))
+    #_(deps/-main "-m" "")
+    (println :PATH (:path dir)))
+  #_(spec/run-spec (.getAbsolutePath (File. "resources/example.tla"))
                  "example.cfg")
   (System/exit 0))
