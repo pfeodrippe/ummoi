43eb4620912aafd95db3d9caa21f0305edc82b7d
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-01 00:06:45 -0300

    gdgs

--- src/ummoi/core.clj
+++ src/ummoi/core.clj
@@ -3,7 +3,8 @@
   (:require
    [clojure.java.shell :as sh]
    [tla-edn.core :as tla-edn]
-   [tla-edn.spec :as spec])
+   [tla-edn.spec :as spec]
+   [clojure.pprint :as pp])
   (:import
    (java.io File)))
 
@@ -12,24 +13,22 @@
 #_"um -cfg p.edn"
 
 #_'{:operators
-  {"TransferMoney"
-   {:args [x y z]
-    :call {:type :shell
-           :command ["bb" "a.clj" x y z]}}}}
-
-#_(spec/defop TransferMoney {:module "example"}
-    [_ _ _ ])
+    {"TransferMoney"
+     {:args [x y z]
+      :call {:type :shell
+             :command ["bb" "a.clj" x y z]}}}}
 
 (def vars-keys
   [:c1 :c2 :account :receiver-new-amount :sender-new-amount :sender
    :receiver :money :pc])
 
+(spec/defop TransferMoney {:module "example"}
+  [a b c]
+  10)
+
 (defn -main
   [& [n]]
   (println :CLASSES @spec/classes-to-be-loaded)
-  (println :LOADED_BEFORE? (spec/classes-loaded?))
-  (spec/compile-operators 'ummoi.core)
-  (println :LOADED_AFTER? (spec/classes-loaded?))
   (spec/run-spec (.getAbsolutePath (File. "resources/example.tla"))
                  "example.cfg"
                  ["-workers" "1"])
